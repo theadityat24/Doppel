353ccea6b698d8c2f4ee8f48281d61ddcd87c68f
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-02-18 20:37:27 -0600

    Added sorting by media type

--- doppel/file_utils.py
+++ doppel/file_utils.py
@@ -1,13 +1,24 @@
-import os, imghdr
+import os, imghdr, shutil
 from pymediainfo import MediaInfo
-
-
 
 class MediaFile:
     def __init__(self, path):
         self.path = path
         info = os.stat(self.path)
         self.type = self.getType(self.path)
+        
+    def move(self, new_dir):
+        shutil.move(self.path, new_dir.path)
+
+    def copy(self, new_dir):
+        shutil.copy2(self.path, new_dir.path)
+
+    def getFileData(self):
+        with open(self.path, self.getFileData) as f:
+            return f.read()
+        
+    def isDuplicateOf(self, f):
+        return self.getFileData() == f.getFileData()
         
     def __str__(self):
         return str(self.path)
@@ -24,7 +35,18 @@ class MediaFile:
                 return 'Video'
             else:
                 return None
-        
-    
+
+class Folder:
+    def __init__(self, path):
+        self.path = path
+        if not os.path.isdir(self.path):
+            os.makedirs(self.path)
+
+    def files(self):
+        f = []
+        for root, dirs, names in os.walk(self.path):
+            for n in names:
+                f.append(MediaFile(os.path.join(root, n)))
+        return f
             
             
--- doppel/main.py
+++ doppel/main.py
@@ -1,7 +1,29 @@
 import os, shutil
+from file_utils import Folder, MediaFile
 
 GET_DIRECTORY = r'D:\Aditya\Pictures Test\From'
 DEST_DIRECTORY = r'D:\Aditya\Pictures Test\To'
 
+def main():
+    get_dir = Folder(GET_DIRECTORY)
+    dest_dir = Folder(DEST_DIRECTORY)
+    videos = Folder(os.path.join(dest_dir.path, 'Videos'))
+    images = Folder(os.path.join(dest_dir.path, 'Images'))
+    audio = Folder(os.path.join(dest_dir.path, 'Audio'))
+    duplicates = Folder(os.path.join(dest_dir.path, 'Duplicates'))
+    other = Folder(os.path.join(dest_dir.path, 'Other'))
+
+    for f in get_dir.files():
+        if f.type is None:
+            f.move(other)
+        elif f.type == 'Video':
+            f.move(videos)
+        elif f.type == 'Image':
+            f.move(images)
+        elif f.type == 'Audio':
+            f.move(audio)
+        else:
+            print(f'something went wrong with {f}')
+
 if __name__ == '__main__':
-    pass
+    main()
